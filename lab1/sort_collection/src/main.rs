2ead324ffbd67a32f09e23501d6dc59cdc1fe647
IP-14Nezdolii <vladislav07007@gmail.com>
2025-09-10 16:19:31 +0300

    new commit

--- lab1/sort_collection/src/main.rs
+++ lab1/sort_collection/src/main.rs
@@ -15,6 +15,16 @@ use std::{cmp::Ordering, hint::black_box};
 const NUM_MEASURES: u64 = 10_000;
 const ARRAY_SIZE: u64 = 1_000_000;
 
+fn main() {
+    let pool = ThreadPoolBuilder::new().num_threads(4).build().unwrap();
+
+    let f64_data = measure_f64(&pool);
+    let string_data = measure_string(&pool);
+
+    write_times(&f64_data, "sort_times1.txt");
+    write_times(&string_data, "sort_times2.txt");
+}
+
 fn measure_time<T>(arr: &mut Vec<T>, cmp: fn(&T, &T) -> Ordering) -> f64 {
     let start = Instant::now();
 
@@ -22,6 +32,38 @@ fn measure_time<T>(arr: &mut Vec<T>, cmp: fn(&T, &T) -> Ordering) -> f64 {
     black_box(&arr);
 
     return start.elapsed().as_nanos() as f64 / 1_000_000.0;
+}
+
+fn measure_f64(pool: &ThreadPool) -> Vec<f64> {
+    pool.install(|| {
+        (0..NUM_MEASURES)
+            .into_par_iter()
+            .map(|i| {
+                let mut rng = rand::rngs::StdRng::seed_from_u64(i);
+                let mut arr = (0..ARRAY_SIZE)
+                    .map(|_| rng.random_range(0.0..=1_000_000.0))
+                    .collect();
+
+                measure_time(&mut arr, f64_cmp)
+            })
+            .collect()
+    })
+}
+
+fn measure_string(pool: &ThreadPool) -> Vec<f64> {
+    pool.install(|| {
+        (0..NUM_MEASURES)
+            .into_par_iter()
+            .map(|i| {
+                let mut rng = rand::rngs::StdRng::seed_from_u64(i);
+                let mut arr = (0..ARRAY_SIZE)
+                    .map(|_| generate_random_string(6, 14, &mut rng))
+                    .collect();
+
+                measure_time(&mut arr, |a: &String, b: &String| a.cmp(b))
+            })
+            .collect()
+    })
 }
 
 #[inline]
@@ -42,51 +84,9 @@ fn generate_random_string(min_len: usize, max_len: usize, rng: &mut StdRng) -> S
     Alphanumeric.sample_string(rng, len)
 }
 
-fn measure_f64(pool: &ThreadPool) {
-    let times: Vec<f64> = pool.install(|| {
-        (0..NUM_MEASURES)
-            .into_par_iter()
-            .map(|i| {
-                let mut rng = rand::rngs::StdRng::seed_from_u64(i);
-                let mut arr = (0..ARRAY_SIZE)
-                    .map(|_| rng.random_range(0.0..=1_000_000.0))
-                    .collect();
-
-                measure_time(&mut arr, f64_cmp)
-            })
-            .collect()
-    });
-
-    let mut file = File::create("sort_times1.txt").unwrap();
+fn write_times(times: &Vec<f64>, filename: &str) {
+    let mut file = File::create(filename).unwrap();
     for time in times.iter() {
         writeln!(file, "{}", time).unwrap();
     }
-}
-
-fn measure_string(pool: &ThreadPool) {
-    let times: Vec<f64> = pool.install(|| {
-        (0..NUM_MEASURES)
-            .into_par_iter()
-            .map(|i| {
-                let mut rng = rand::rngs::StdRng::seed_from_u64(i);
-                let mut arr = (0..ARRAY_SIZE)
-                    .map(|_| generate_random_string(6, 14, &mut rng))
-                    .collect();
-
-                measure_time(&mut arr, |a: &String, b: &String| a.cmp(b))
-            })
-            .collect()
-    });
-
-    let mut file = File::create("sort_times2.txt").unwrap();
-    for time in times.iter() {
-        writeln!(file, "{}", time).unwrap();
-    }
-}
-
-fn main() {
-    let pool = ThreadPoolBuilder::new().num_threads(4).build().unwrap();
-
-    measure_f64(&pool);
-    measure_string(&pool);
 }
